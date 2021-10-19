f326f113c383beaa563aa456c1d91ae0888c40b6
blbadger <blbadger10@gmail.com>
2021-10-18 22:22:32 -0400

    cleaned clifford zoom2

--- clifford_bound_zoom2.py
+++ clifford_bound_zoom2.py
@@ -1,27 +1,50 @@
-#! Python3
+# clifford_bound_zoom2.py
+
 # A program that displays an evolution of the clifford attractor
 # using Euler's formula with deliberately large values of delta_t
 # combined with a quiver plot, iterating over larger and larger
 # values of delta_t.  Produces a series of .png images that can be 
 # assembled into a movie.
 
+
 # import third party libraries
 import numpy as np 
 import matplotlib.pyplot as plt 
+
+import copy
 plt.style.use('dark_background')
 
 def clifford_attractor(x, y, a=-1.4, b=1.7, c=1.0, d=0.7):
-	'''Returns the change in arguments x and y according to 
-	the Clifford map equation. Kwargs a, b, c, and d are specified
-	as constants.
-	'''
+	"""
+	Returns the next iterations of the Clifford map.
+
+	Args:
+		x, y: float, current x and y values
+		a, b, d, c: float, constant parameters
+
+	Returns:
+		x_next, y_next: float, next x and y values
+
+	"""
 	x_next = np.sin(a*y) + c*np.cos(a*x) 
 	y_next = np.sin(b*x) + d*np.cos(b*y)
+
 	return x_next, y_next
 
-import copy
 
 def clifford_boundary(max_iterations, t, a=-1.4, b=1.7, c=1.0, d=0.7):
+	"""
+	Plot the boundary of the Clifford attractor near (x, y) = (9.82, 7.86)
+
+	Args:
+		max_iterations: int, number of steps iterating Clifford eqns.
+		t: int, time point
+		a, b, c, d: float, constant parameters
+
+	Returns:
+		iterations_in_basin[0]: np.meshgrid array
+
+	"""
 	x_range = 3000
 	y_range = 3000
 
@@ -32,6 +55,7 @@ def clifford_boundary(max_iterations, t, a=-1.4, b=1.7, c=1.0, d=0.7):
 	x2 = np.zeros(x_range)
 	y2 = np.zeros(y_range)
 	iterations_until_in_basin = np.meshgrid(x2, y2)
+
 	for i in iterations_until_in_basin:
 		for j in i:
 			j += max_iterations
@@ -39,7 +63,9 @@ def clifford_boundary(max_iterations, t, a=-1.4, b=1.7, c=1.0, d=0.7):
 	not_already_in_basin = iterations_until_in_basin[0] < 10000
 
 	for k in range(max_iterations):
-		array_copied = copy.deepcopy(array[0]) # copy array to prevent premature modification of x array
+
+		# copy array to prevent premature modification of x array
+		array_copied = copy.deepcopy(array[0]) 
 
 		# henon map applied to array 
 		array[0] = array[0] + (1.35)*(np.sin(a*array[1]) + c*np.cos(a*array[0]))
@@ -53,9 +79,9 @@ def clifford_boundary(max_iterations, t, a=-1.4, b=1.7, c=1.0, d=0.7):
 
 	return iterations_until_in_basin[0]
 
-	
-# differential trajectory
-for t in range(, 500):
+
+# create video
+for t in range(500):
 
 	# number of iterations
 	iterations = 100000
